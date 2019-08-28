ae215e9464ac8259daf10a71c03db733c2cc303e
lev <mail@levmorozov.ru>
2019-08-28 18:58:14 +0300

    more convenient naming

--- dbdump.sh
+++ dbdump.sh
@@ -2,7 +2,7 @@
 
 DBNAME=$1
 OUTPATH=$2
-FILENAME=`date +%F-%H%M-`$HOSTNAME-$DBNAME".sql.gz.enc"
+FILENAME=$DBNAME-`date +%F-%H%M-`$HOSTNAME".sql.gz.enc"
 
 mysqldump --quick --lock-tables $DBNAME | gzip -5 | openssl enc -e -aes-256-cbc -md sha256 -pbkdf2 -kfile $HOME/.backuppass > $OUTPATH/$FILENAME
 
