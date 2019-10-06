e97d16a520bab5e7072a3f6c7b12d40a9cfa44f8
levmorozov <mail@levmorozov.ru>
2019-10-06 16:02:31 +0300

    Fix

--- debian/php.sh
+++ debian/php.sh
@@ -29,8 +29,8 @@ sed -i 's/;realpath_cache_size\s=.*/realpath_cache_size = '128k'/' /etc/php/7.3/
 sed -i 's/;realpath_cache_ttl\s=.*/realpath_cache_ttl = '3600'/' /etc/php/7.3/fpm/php.ini
 sed -i -E 's/;?expose_php = .*/expose_php = Off/' /etc/php/7.3/fpm/php.ini
 sed -i -E 's/;?session.use_strict_mode\s=.*/session.use_strict_mode = 1/' /etc/php/7.3/fpm/php.ini
-sed -i -E 's/;?opcache.memory_consumption\s=.*/opcache.memory_consumption = 64/' /etc/php/7.3/fpm/php.ini
-sed -i -E 's/;?opcache.validate_timestamps\s=.*/opcache.validate_timestamps = 0/' /etc/php/7.3/fpm/php.ini
+sed -i -E 's/;?opcache.memory_consumption\s?=.*/opcache.memory_consumption = 64/' /etc/php/7.3/fpm/php.ini
+sed -i -E 's/;?opcache.validate_timestamps\s?=.*/opcache.validate_timestamps = 0/' /etc/php/7.3/fpm/php.ini
 sed -i -E 's/;?opcache.save_comments\s*=.*/opcache.save_comments = 0/' /etc/php/7.3/fpm/php.ini
 sed -i -E 's/;?date.timezone =/date.timezone = 'Europe\\/Moscow'/' /etc/php/7.3/fpm/php.ini
 sed -i -E 's/;?date.timezone =/date.timezone = 'Europe\\/Moscow'/' /etc/php/7.3/cli/php.ini
