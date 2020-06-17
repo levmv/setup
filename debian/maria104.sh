8aed9a8c3d219f41fcdbcdd68b07d22c3cbebce4
lev <mail@levmorozov.ru>
2020-06-17 16:00:16 +0300

    up

--- debian/maria104.sh
+++ debian/maria104.sh
@@ -17,7 +17,7 @@ password=$MYSQLBACKUPPASS
 EOF
 
 
-apt-get install software-properties-common mariadb-devel
+apt-get install software-properties-common
 add-apt-repository 'deb [arch=amd64] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.4/debian buster main'
 
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
@@ -27,14 +27,14 @@ apt update
 debconf-set-selections <<< "mariadb-server-10.4 mysql-server/root_password password ${MYSQLROOTPASS}"
 debconf-set-selections <<< "mariadb-server-10.4 mysql-server/root_password_again password ${MYSQLROOTPASS}"
 
-apt-get install -y mariadb-server mariadb-client
+apt-get install -y mariadb-server mariadb-client libmariadb-dev
 
-mysql --user=root -p$MYSQLROOTPASS <<_EOF_
+mysql --user=root <<_EOF_
   DELETE FROM mysql.user WHERE User='';
   DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
   DROP DATABASE IF EXISTS test;
   DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-
+  SET password = PASSWORD($MYSQLROOTPASS);
   GRANT LOCK TABLES, SELECT ON *.* TO 'backupuser'@'127.0.0.1' IDENTIFIED BY '$MYSQLBACKUPPASS';
 
   FLUSH PRIVILEGES;
