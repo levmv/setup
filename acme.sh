c4d1da0600a15aa123acbaadcc3acf30ed31c2c4
Lev Morozov <lev@artaill.com>
2020-09-25 16:34:41 +0300

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-
+set -euxo pipefail
 
 curl https://get.acme.sh | sh
-.acme.sh/acme.sh --register-account | grep ACCOUNT_THUMBPRINT | awk -F "=" '{print $2}' | tr -d \' > thumbprint
-cat <<EOF > /etc/nginx/acme
+.acme.sh/acme.sh --register-account | grep ACCOUNT_THUMBPRINT | awk -F "=" '{print $2}' | tr -d \' | sudo tee thumbprint
+sudo tee /etc/nginx/acme <<EOF
 location /.well-known {
     location ~ ^/\.well-known/acme-challenge/([-_a-zA-Z0-9]+)$ {
         default_type text/plain;
-        return 200 "\$1.$(cat /root/thumbprint)";
+        return 200 "\$1.$(cat ~/thumbprint)";
     }
 }
 EOF
