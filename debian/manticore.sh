fd684a0fc4a4a18c3d71acd981aeead7009eaa47
lev <mail@levmorozov.ru>
2022-11-11 21:19:39 +0300

    lang packs

--- debian/manticore.sh
+++ debian/manticore.sh
@@ -5,3 +5,6 @@ sudo dpkg -i manticore-repo.noarch.deb
 sudo apt update
 
 sudo apt install -y manticore
+
+wget -qO- https://repo.manticoresearch.com/repository/morphology/ru.pak.tgz  | sudo tar zxv -C /usr/share/manticore/
+wget -qO- https://repo.manticoresearch.com/repository/morphology/en.pak.tgz  | sudo tar zxv -C /usr/share/manticore/
