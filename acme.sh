7a7a7fc3d988c6628c180d76432bebc90f84b6d6
lev <mail@levmorozov.ru>
2022-11-19 13:00:18 +0300

    fix

--- acme.sh
+++ acme.sh
@@ -2,14 +2,14 @@
 set -euxo pipefail
 
 curl https://get.acme.sh | sh
-/root/.acme.sh/acme.sh --set-default-ca  --server letsencrypt
-/root/.acme.sh/acme.sh --register-account --accountemail | grep ACCOUNT_THUMBPRINT | awk -F "=" '{print $2}' | tr -d \' | tee thumbprint
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+THUMBPRINT=$(/root/.acme.sh/acme.sh --register-account --accountemail | grep ACCOUNT_THUMBPRINT | awk -F "=" '{print $2}' | tr -d \')
+
 tee /etc/nginx/acme <<EOF
 location /.well-known {
     location ~ ^/\.well-known/acme-challenge/([-_a-zA-Z0-9]+)$ {
         default_type text/plain;
-        return 200 "\$1.$(cat ~/thumbprint)";
+        return 200 "\$1.$THUMBPRINT";
     }
 }
 EOF
-rm thumbprint
